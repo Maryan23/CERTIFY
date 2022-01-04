8626cee89c3a3c86d7242258d816ad7f4be8bdd6
Kevson102 <nyambura.njuguna@student.moringaschool.com>
2022-01-04 14:03:17 +0300

    CREATES A SUPERUSER AND REGISTERS THE MODELS IN THE ADMIN

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Institution, Learner
 
 # Register your models here.
+admin.site.register(Institution)
+admin.site.register(Learner)
--- api/models.py
+++ api/models.py
@@ -4,29 +4,6 @@ from cloudinary.models import CloudinaryField
 from django.db import models
 
 # Create your models here.
-# class User(AbstractUser):
-#     is_employer = models.BooleanField(default=False)
-
-# class Employer(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-#     company_name = models.CharField(max_length=50,null=True,blank = True)
-#     prof_photo = CloudinaryField('image')
-#     about = models.TextField(max_length=1000, blank=True, null=True)
-#     contact = models.CharField(max_length=50, blank=True, null=True)
-
-#     def __str__(self):
-#         return self.company_name
-
-#     def save_employer(self):
-#         self.save()
-
-#     def update_employer(self):
-#         self.save()
-
-#     @classmethod
-#     def search_by_company_name(cls,search_term):
-#         employer = cls.objects.filter(project_name__icontains=search_term)
-#         return employer
 
 class Institution(models.Model):
     institution_reg_no = models.CharField(max_length=30)
@@ -61,6 +38,11 @@ class Learner(models.Model):
     
     def delete_learner(self):
         self.delete()
+    
+    # @classmethod    
+    # def view_learners_by_institution(cls, institution_search):
+    #     learners_by_institution = cls.objects.filter(institution_search)
+    #     return learners_by_institution
         
     def __str__(self):
         return self.learner_first_name
