b59e6f23ef725014184efa34e7fd0b99a70005ac
mohamedissack <mohamed.abdi@student.moringashool.com>
2022-01-07 08:53:18 +0300

    update serializers

--- api/serializer.py
+++ api/serializer.py
@@ -7,3 +7,4 @@ class InstitutionSerializer(serializers.ModelSerializer):
 class LearnerSerializer(serializers.ModelSerializer):
   class Meta:
     model = Learner
+    fields = ('learner_reg_no', 'learner_first_name', 'learner_second_name', 'learner_last_name', 'course_taken', 'date_enrolled', 'date_completed', 'grade_attained', 'certificate_image', 'institution')
