2d7ef5f35c155a76040b731f4e0db2c400c1df22
mohamedissack <mohamed.abdi@student.moringashool.com>
2022-01-07 08:52:13 +0300

    update serializers

--- api/serializer.py
+++ api/serializer.py
@@ -3,3 +3,4 @@ from .models import Institution, Learner
 class InstitutionSerializer(serializers.ModelSerializer):
   class Meta:
     model = Institution
+    fields = ('institution_reg_no', 'institution_name', 'location_name', 'location_address', 'institution_email')
