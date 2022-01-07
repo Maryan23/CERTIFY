a9c7fbff0743715c520a577bdc66b5d11db928a9
mohamedissack <mohamed.abdi@student.moringashool.com>
2022-01-07 09:58:58 +0300

    update views learner

--- api/views.py
+++ api/views.py
@@ -5,9 +5,11 @@ from django.contrib.auth.decorators import login_required
 from rest_framework import serializers, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from api.models import Learner
+from .serializer import LearnerSerializer
 # Create your views here.
 class LearnerstList(APIView):
   def get(self, request, format=None):
-    all_learners = Project.objects.all()
-    serializers=LearnersSerializer(all_learners, many=True)
+    all_learners = Learner.objects.all()
+    serializers=LearnerSerializer(all_learners, many=True)
     return Response(serializers.data)
