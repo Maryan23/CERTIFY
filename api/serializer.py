47c319f0321ac1fabc88166ac68086b79291e792
mohamedissack <mohamed.abdi@student.moringashool.com>
2022-01-07 08:53:52 +0300

    update serializers

--- api/serializer.py
+++ api/serializer.py
@@ -9,3 +9,4 @@ class LearnerSerializer(serializers.ModelSerializer):
     model = Learner
     fields = ('learner_reg_no', 'learner_first_name', 'learner_second_name', 'learner_last_name', 'course_taken', 'date_enrolled', 'date_completed', 'grade_attained', 'certificate_image', 'institution')
 from .models import Employer
+class EmployerSerializer(serializers.ModelSerializer):
