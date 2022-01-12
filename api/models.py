dceb53dcca70f3be4bd6ca5f0d084d2e0b184d37
Maryan23 <maryann.mwikali@student.moringaschool.com>
2022-01-12 22:23:18 +0300

    Revert to django frontend

--- api/models.py
+++ api/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from cloudinary.models import CloudinaryField
 from django.db import models
+from django.db.models.deletion import CASCADE
+from django.db.models.lookups import In
 
 # Create your models here.
 class User(AbstractUser):
@@ -10,13 +12,34 @@ class User(AbstractUser):
     is_learner = models.BooleanField(default = False)
 
 
+class Employer(models.Model):
+    logo = CloudinaryField('logo')
+    company_name = models.CharField(max_length=50)
+    about = models.TextField(max_length=1000, blank=True)
+    tel_number = models.IntegerField()
+    email = models.EmailField()
+    reg_number = models.CharField(max_length=20)
+    joined_on = models.DateTimeField(auto_now_add=True,null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE,null=True)
+
+    def save_employer(self):
+        self.save()
+    
+    def update_employer(self):
+        self.save()
+    
+    def __str__(self):
+        return self.company_name
+
+
 class Institution(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    institution_reg_no = models.CharField(max_length=30)
+    reg_no = models.CharField(max_length=30)
     institution_name = models.CharField(max_length=40)
-    location_name = models.CharField(max_length=20)
+    location = models.CharField(max_length=20)
     location_address = models.CharField(max_length=20)
-    institution_email = models.EmailField()
+    email = models.EmailField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+
     
     def save_institution(self):
         self.save()
@@ -26,50 +49,45 @@ class Institution(models.Model):
     
     def __str__(self):
         return self.institution_name
+    
+    @classmethod
+    def get_institution_by_name(cls, search_term):
+        institution = cls.objects.filter(name__icontains=search_term)
+        return institution
 
-class Employer(models.Model):
-    company_logo = CloudinaryField('image')
-    company_name = models.CharField(max_length=50)
-    about = models.TextField(max_length=1000, blank=True)
-    company_tel_number = models.IntegerField()
-    company_email = models.EmailField()
-    company_reg_number = models.CharField(max_length=20)
-    joined_on = models.DateTimeField(auto_now_add=True,null=True)
-    user = models.OneToOneField(User, on_delete=models.CASCADE,null=True)
+class Certificate(models.Model):
+    cert_name = models.CharField(max_length=30,null=True)
+    cert_image = CloudinaryField('cert_image')
+    institution = models.ForeignKey(Institution, on_delete=CASCADE , null=True)
 
-
-    def save_employer(self):
+    def save_certificate(self):
         self.save()
     
-    def update_employer(self):
-        self.save()
+    def delete_certificate(self):
+        self.delete()
+    
+    def __str__(self):
+        return self.cert_name
 
-    @classmethod
-    def search_by_company_name(cls,search_term):
-        employer = cls.objects.filter(project_name__icontains=search_term)
-        return employer
     
 class Learner(models.Model):
     learner_image = CloudinaryField('learner_image')
-    learner_reg_no = models.CharField(max_length=30)
-    learner_first_name = models.CharField(max_length=10)
-    learner_second_name = models.CharField(max_length=10, blank=True)
-    learner_last_name = models.CharField(max_length=10)
+    reg_no = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=10)
+    second_name = models.CharField(max_length=10, blank=True)
+    last_name = models.CharField(max_length=10)
     course_taken = models.CharField(max_length=30)
     date_enrolled = models.DateTimeField()
     date_completed = models.DateTimeField()
     grade_attained = models.CharField(max_length=20)
-    certificate_image = CloudinaryField('image')
-    institution = models.ManyToManyField(Institution)
+    certificates = models.ForeignKey(Certificate , on_delete=CASCADE , null=True)
+    institution = models.ForeignKey(Institution, on_delete=CASCADE , null=True)
     def _str_(self):
-        return self.learner_first_name 
+        return self.first_name 
     
     def save_learner(self):
         self.save()
     
     def delete_learner(self):
-        self.delete()
-    
-    def __str__(self):
-        return self.learner_first_name
+        self.delete()   
     
--- api/urls.py
+++ api/urls.py
@@ -1,20 +1,9 @@
 from django.urls import path,include
 from django.conf.urls import url
-from rest_framework import routers
 from . import views
-from rest_framework.authtoken.views import ObtainAuthToken
-
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
 
 
 urlpatterns=[
-    path('', include(router.urls)),
-    path('api/employers/<int:id>/',views.EmployerList),
-    path('register/', include('rest_framework.urls', namespace='rest_framework')),
-    path('auth/',ObtainAuthToken.as_view()),
-    url(r'^institutions/api/$', views.institutionAPI),
-    url(r'^institutions/api/([0-9]+)$', views.institutionAPI),
-    url(r'^learners/api/$', views.LearnerAPI),
-    url(r'^learners/api/([0-9]+)$', views.LearnerAPI),
+    path('' ,views.index ,name='index')
+
 ]
